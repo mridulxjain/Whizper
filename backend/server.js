1d4754322aeb04ba6455f609f8eb53c9b4ff3aed
mridulxjain <krishnajain2604@gmail.com>
2026-01-07 13:52:42 +0530

    fix CORS error

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,18 @@ const adminRouter = require("./src/routes/adminRouter");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://whizper.vercel.app"
+  ],
+  methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 app.use("/api/confessions", confessionRouter);
